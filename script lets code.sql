43cce5011e2f3113507c52bd593721945341163e
RhuanGabriel1 <74109343+RhuanGabriel1@users.noreply.github.com>
2022-03-23 13:54:28 -0300

    adicionando tabulação

--- script lets code.sql
+++ script lets code.sql
@@ -1,44 +1,43 @@
-
 create table cliente(
-id int not null primary key,
+	id int not null primary key,
 
-nome varchar(100) not null,
-nascimento date not null,
-email varchar(45)
+	nome varchar(100) not null,
+	nascimento date not null,
+	email varchar(45)
 );
 
 create table dependente(
-id int not null primary key,
+	id int not null primary key,
 
-titular_id int not null,
-dependente_id int not null,
+	titular_id int not null,
+	dependente_id int not null,
 
-constraint titular_id  foreign key (titular_id) references cliente(id),
-constraint dependente_id foreign key (dependente_id) references Cliente(id)
+	constraint titular_id  foreign key (titular_id) references cliente(id),
+	constraint dependente_id foreign key (dependente_id) references Cliente(id)
 );
 
 create table produto(
-ans_id int primary key not null,
+	ans_id int primary key not null,
 
-descricao varchar(250) not null, 
-valor decimal (5,2) not null
+	descricao varchar(250) not null, 
+	valor decimal (5,2) not null
 );
 
 create table contrato(
-id int not null primary key,
+	id int not null primary key,
 
-data_de_inicio date not null,
-titular_id int not null,
+	data_de_inicio date not null,
+	titular_id int not null,
 
-constraint titular_id  foreign key (titular_id) references cliente(id)
+	constraint titular_id  foreign key (titular_id) references cliente(id)
 );
 
 create table contrato_produto(
-id int not null primary key,
+	id int not null primary key,
 
-contrato_id int not null ,
-produto_id int not null ,
+	contrato_id int not null ,
+	produto_id int not null ,
 
-constraint contrato_id foreign key (contrato_id) references contrato(id),
-constraint produto_id foreign key (produto_id) references produto(ans_id)
+	constraint contrato_id foreign key (contrato_id) references contrato(id),
+	constraint produto_id foreign key (produto_id) references produto(ans_id)
 );
